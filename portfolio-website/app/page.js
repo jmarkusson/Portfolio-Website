dc5ee5821b2c2e0841193ed61870f422f5c47f1f
jmarkusson <jomarkusson@gmail.com>
2025-01-30 11:48:18 +0100

    initial setup

---
+++ portfolio-website/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col bg-[#121212]">
+       <h1>Portfolio Website</h1>
+    </div>
+  );
+}
